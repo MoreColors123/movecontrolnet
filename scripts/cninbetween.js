83af636853df52c17753de95b3965357ff2e3485
MoreColors123 <131474336+MoreColors123@users.noreply.github.com>
2023-05-05 17:12:12 +0200

    Add files via upload
    Puts the controlnet-menu between the settings and the image results.

---
+++ scripts/cninbetween.js
@@ -0,0 +1,30 @@
+/* globals jQuery, $, waitForKeyElements */
+// ==UserScript==
+// @name         Controlnet-Inbetween
+// @version      1
+// @description  Example script to move an HTML element with jQuery
+// @match        *://127.0.0.1:7860/*
+// @grant        none
+// @require      https://code.jquery.com/jquery-3.6.0.min.js
+// @run-at   document-start
+// @sandbox JavaScript
+// ==/UserScript==
+
+
+(function() {
+    'use strict';
+
+    $(window).on('load', function() {
+        // move controlnet in txt2img-tab
+        var cnt2i = $('#txt2img_controlnet');
+        var txt2img_res = $('#txt2img_results');
+        cnt2i.insertBefore(txt2img_res);
+
+        // move controlnet in img2img-tab
+        var cni2i = $('#img2img_controlnet');
+        var img2img_res = $('#img2img_results');
+        cni2i.insertBefore(img2img_res);
+
+    });
+
+})();
